7de93ea262665146b0ccc2165db9dd1fe5fba298
Sandra86Martinez <sandymartinez.186@gmail.com>
2025-05-05 20:42:52 -0300

    Agregué una variable nueva llamada materia al archivo .py

--- repo_prueba_1.py
+++ repo_prueba_1.py
@@ -2,5 +2,6 @@ nombre="Sandra"
 apellido="Martinez"
 edad=38
 ciudad="Bs. As."
+materia="Programación I"
 
-print(f"Hola soy {nombre} {apellido}, tengo {edad}. Vivo en {ciudad}.")
+print(f"Hola soy {nombre} {apellido}, tengo {edad}. Vivo en {ciudad} y soy alumna de la materia {materia}.")
